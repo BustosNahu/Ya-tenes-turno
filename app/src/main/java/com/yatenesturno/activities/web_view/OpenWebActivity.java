0a326d71df72718c4ec22608d4a78a927ccdd6e8
BustosNahu <nbustosalexander@gmail>
2023-04-05 14:24:41 -0300

    TERMINADO con anim y web screen se ve bien

--- app/src/main/java/com/yatenesturno/activities/web_view/OpenWebActivity.java
+++ app/src/main/java/com/yatenesturno/activities/web_view/OpenWebActivity.java
@@ -3,15 +3,13 @@ package com.yatenesturno.activities.web_view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageButton;
+
 import android.widget.ImageView;
 
 import com.yatenesturno.R;
@@ -30,7 +28,7 @@ public class OpenWebActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_open_web);
+        setContentView(R.layout.open_web_layout);
 
         getSupportActionBar().setBackgroundDrawable(getDrawable(R.color.white));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
